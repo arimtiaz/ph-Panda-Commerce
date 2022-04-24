1a5f465a9eb572b26e01db11240bb602d9b6324f
arimtiaz <abadur391@gmail.com>
2022-04-25 03:07:14 +0600

    Updating

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
-const coloringTitle = document.getElementsByClassName(coloring)
-coloringTitle.style.color ="Blue";
+const shoeColor = document.getElementById("ShoeBackground");
+shoeColor.style.backgroundColor = "salmon";
+
+// const niceCards = document.getElementsByClassName("card101");
+// document.getElementById('card101').style.borderRadius = '3em';
