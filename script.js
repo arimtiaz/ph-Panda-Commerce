22fc288b6e43e6644ba7945a7e8c555c0949f9e1
arimtiaz <abadur391@gmail.com>
2022-04-25 03:46:28 +0600

    back in the game

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ shoeColor.style.backgroundColor = "salmon";
 
 // const niceCards = document.getElementsByClassName("card101");
 // document.getElementById('card101').style.borderRadius = '3em';
+
+function b1(){
+    document.getElementById("thisButton").style.display="none";
+}
+
+function onHover()
+{
+    $("#profileCover").attr('src', 'images/pakainfo.png');
+}
+
+function offHover()
+{
+    $("#profileCover").attr('src', 'images/infinityknow.png');
+}
